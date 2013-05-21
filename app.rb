bebedd46513948494ed24296f24ad6db94b61ea3
Jarmo Isotalo <jamo@isotalo.fi>
2013-05-21 16:58:10 +0300

    debug

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+set :root, File.dirname(__FILE__)
+enable  :sessions, :logging
+
+class App < Sinatra::Base
+  get '/' do
+    "#{request.inspect}"
+  end
+
+
+  @repo_url = "https://github.com/jamox/Kandi.git"
+  post '/' do
+    push = JSON.parse(params[:payload])
+    `cd /home/jamo/sites/root/kandi/ && git pull && cd referaatti/ && pdflatex template-fi.tex`
+  end
+end
